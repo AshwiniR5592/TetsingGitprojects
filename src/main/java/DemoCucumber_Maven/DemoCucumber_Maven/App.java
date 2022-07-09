bd4dfe3719075d401c43bdca605637047400f79e
AshwiniR5592 <ashwinirathod5592@gmail.com>
2022-07-09 12:22:38 -0700

    first commit

---
+++ src/main/java/DemoCucumber_Maven/DemoCucumber_Maven/App.java
@@ -0,0 +1,13 @@
+package DemoCucumber_Maven.DemoCucumber_Maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ target/test-classes/com/jbk/Login.feature
@@ -0,0 +1,8 @@
+Feature: Login functionality
+
+Scenario: To check login functionality 
+
+Given : When user open site 
+When : When user enters username and password 
+Then :user navigate to home page
+ 
